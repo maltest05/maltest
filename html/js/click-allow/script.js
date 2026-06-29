b2c32c9bccaf70eb2f05a6fd230458e9c44e628e
maltest05 <onegrandband@gmail.com>
2026-06-29 17:33:38 -0500

    Refactor notification permission request logic

--- html/js/click-allow/script.js
+++ html/js/click-allow/script.js
@@ -1,14 +1,31 @@
 function triggerNotificationPrompt() {
-  // Check if browser supports notifications
-  if ('Notification' in window) {
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      } else {
-        console.log('Notification permission denied.');
-      }
+
+    if (!("Notification" in window)) {
+
+        alert("This browser doesn't support notifications.");
+
+        return;
+
+    }
+
+    Notification.requestPermission().then(function(permission){
+
+        if(permission === "granted"){
+
+            console.log("Notification permission granted.");
+
+        }
+        else if(permission === "denied"){
+
+            console.log("Notification permission denied.");
+
+        }
+        else{
+
+            console.log("Notification dismissed.");
+
+        }
+
     });
-  } else {
-    console.log('Click "Allow"');
-  }
+
 }
